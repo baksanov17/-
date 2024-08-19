764f637158b078cb8624447139f6361bc5e25d57
baksanov17 <baksanov17@mail.ru>
2024-08-20 02:04:47 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+number_of_completed_tasks = 12
+amount_of_time_spent = 1.5
+course_name = 'Python'
+time_for_one_task = amount_of_time_spent / number_of_completed_tasks
+print("Курс:",course_name,",всего задач:",number_of_completed_tasks,",затрачено часов:",amount_of_time_spent)
+print(",среднее время выполнения",time_for_one_task)
